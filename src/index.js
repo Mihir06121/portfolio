ec56915475aaf416b5ee94763c1be6d9d9a4ed67
mihir <mp06121@gmail.com>
2022-07-15 10:05:24 +0530

    major Update commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from "react-dom/client";
 import {
-  BrowserRouter,
+  HashRouter,
   Routes,
   Route,
 } from "react-router-dom";
@@ -15,7 +15,7 @@ const root = ReactDOM.createRoot(
   document.getElementById("root")
 );
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="/" element={<App />} />
       {/* <Route path="blogs" element={<Blogs />} />
@@ -24,5 +24,5 @@ root.render(
         <Route path=":invoiceId" element={<Invoice />} />
       </Route>
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
