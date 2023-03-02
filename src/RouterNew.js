3040c6ea1d8cae2c7afe4b96281951dbd39f4e40
Mihir <mp06121@gmail.com>
2023-03-02 19:19:27 +0530

    router-test-2

--- src/RouterNew.js
+++ src/RouterNew.js
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
-import About from "./Home";
+import About from "./About";
 // import { ParallaxProvider } from 'react-scroll-parallax'
 
 const NavRoutes = () => (
